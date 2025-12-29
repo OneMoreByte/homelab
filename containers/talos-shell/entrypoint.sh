e13c089daf79db4c36390a7e3b9ca19a96fb03c5
Jack Hildebrandt <jack@jackhil.de>
2025-12-29 14:16:24 -0600

    rework some more stuff

---
+++ containers/talos-shell/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/fish
+
+tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Dotted --prompt_spacing=Compact --icons='Few icons' --transient=Yes
+
+/usr/bin/fish
