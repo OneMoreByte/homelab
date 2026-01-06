f6db3084593d61b743c6c1c346cbc5e899f43fe7
Jack Hildebrandt <jack@jackhil.de>
2026-01-05 20:08:26 -0600

    hehehe

---
+++ containers/backup-target/configure-user.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+USER=$1
+
+if [ -z $USER ]; then
+  echo "invalid string $USER"
+  exit 1
+fi
+
+if ! id "${USER}" &>/dev/null; then
+  echo "user isn't configured in image";
+  exit 1
+fi
+
+DATASET="store/backups/foreign/${USER}"
+DATASET_PERMISSIONS="create,mount,snapshot,rollback,clone,promote,rename,receive,send,hold,release,diff"
+CHILDREN_ONLY_PERMISSIONS="destroy"
+
+if ! zfs allow $DATASET | grep -q "${USER}"; then
+  zfs allow "${USER}" "${DATASET_PERMISSIONS}" "${DATASET}"
+  zfs allow -d "${USER}" "${CHILDREN_ONLY_PERMISSIONS}" "${DATASET}"
+fi
